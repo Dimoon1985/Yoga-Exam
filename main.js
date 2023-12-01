c5f39c244939f99d5d1ef5f70168b1aa22e4b4d3
Dimoon1985 <137377936+Dimoon1985@users.noreply.github.com>
2023-12-01 21:07:54 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// import JustValidate from "just-validate";
+
 // Список вопросов
 
 const questions = [
@@ -57,8 +59,6 @@ const questions = [
   },
 ];
 
-// Таймер
-
 const min = document.querySelector(".min");
 const sec = document.querySelector(".sec");
 const milSec = document.querySelector(".mil-sec");
@@ -68,6 +68,8 @@ const btnGenerate = document.querySelectorAll(".button");
 const btnReset = document.getElementById("reset");
 const timerDisplay = document.getElementById("display");
 const btnReady = document.getElementById("ready");
+
+// Таймер
 
 let interval;
 let minutes = 0;
@@ -102,20 +104,8 @@ function startTimer() {
 
   milSec.innerHTML = milliseconds < 10 ? "0" + milliseconds : milliseconds;
 }
-for (btn of btnGenerate) {
-  btn.addEventListener("click", function () {
-    let card = this.closest(".card");
-    console.log(card);
-    clearInterval(interval);
-    interval = setInterval(startTimer, 10);
-  });
-}
-// btnGenerate.addEventListener("click", () => {
-//   clearInterval(interval);
-//   interval = setInterval(startTimer, 10);
-// });
 
-btnReset.addEventListener("click", () => {
+btnReset.addEventListener("click", function () {
   clearInterval(interval);
   minutes = 0;
   seconds = 0;
@@ -125,15 +115,67 @@ btnReset.addEventListener("click", () => {
   sec.innerHTML = "00";
   milSec.innerHTML = "00";
 });
-btnReady.addEventListener("click", () => {
+btnReady.addEventListener("click", function () {
   clearInterval(interval);
 });
+// КОНЕЦ ТАЙМЕРА
 
-// ВАЛИДАЦИЯ
-// const form = document.getElementById("add-form");
-// const input = document.getElementById("inp");
+// ФУНКЦИЯ СТАРТА ТАЙМЕРА И КАРТОЧКИ ПО КНОПКЕ "СГЕНЕРИРОВАТЬ"
+function counterStartCard() {
+  for (const btn of btnGenerate) {
+    btn.addEventListener("click", function (e) {
+      let card = this.closest(".card");
+      clearInterval(interval);
+      interval = setInterval(startTimer, 10);
+    });
+  }
+}
 
-// function validation(form) {
+// КОНЕЦ ФУНКЦИИ
+
+//ВАЛИДАЦИЯ;
+const form = document.querySelectorAll(".form");
+const inputs = document.querySelectorAll(".input");
+
+function validation() {
+  function removeErrors(input) {
+    const parent = input.parentNode;
+    if (parent.classList.contains("error")) {
+      parent.querySelector(".error-label").remove(".error-label");
+      parent.classList.remove("error");
+    }
+  }
+
+  function createError(input, text) {
+    const parent = input.parentNode;
+    const errorLabel = document.createElement("label");
+
+    errorLabel.classList.add("error-label");
+    errorLabel.textContent = text;
+
+    parent.classList.add("error");
+    parent.append(errorLabel);
+  }
+
+  for (const inp of inputs) {
+    inp.addEventListener("input", function (e) {
+      removeErrors(inp);
+      if (e.target.value === "") {
+        createError(inp, "Поле не заполнено!");
+      } else if (!Number(e.target.value)) {
+        createError(inp, "Введены буквы, а надо цифры!");
+      } else if (e.target.value.length >= 4) {
+        createError(inp, "Слишком много символов!");
+      } else {
+        counterStartCard();
+      }
+    });
+  }
+}
+validation();
+
+//Валидация 1
+// function validation() {
 //   function removeError() {
 //     if (input.classList.contains("error-label")) {
 //       document.querySelector("label").remove();
@@ -150,37 +192,139 @@ btnReady.addEventListener("click", () => {
 //     console.log(errorLabel);
 //   }
 
-//   btnGenerate.addEventListener("click", () => {
-//     removeError();
-//     if (input.value === "") {
-//       createError("Поле не заполнено!");
-//       clearInterval(interval);
-//     } else if (!Number(input.value)) {
-//       createError("Введены буквы, а надо цифры!");
-//       clearInterval(interval);
-//     }
-//   });
+// btnGenerate.addEventListener("click", () => {
+//   removeError();
+//   if (input.value === "") {
+//     createError("Поле не заполнено!");
+//     clearInterval(interval);
+//   } else if (!Number(input.value)) {
+//     createError("Введены буквы, а надо цифры!");
+//     clearInterval(interval);
+//   }
+// });
 // }
+// validation();
+
 // validation(this);
-function validation(form) {
-  let result = true;
+// function validation(form) {
+//   let result = true;
 
-  const card = document.querySelectorAll(".card");
-  console.log(card);
-  card.forEach((i) => {
-    console.log(i.children[0][0]);
-  });
+//   const card = document.querySelectorAll(".card");
+//   console.log(card);
+//   card.forEach((i) => {
+//     console.log(i.children[0][0]);
+//   });
 
-  // form.querySelectorAll("input").forEach((input) => {
-  //   console.log(input);
-  // });
+//   // form.querySelectorAll("input").forEach((input) => {
+//   //   console.log(input);
+//   // });
 
-  return result;
-}
+//   return result;
+// }
 
-document.querySelectorAll(".form").addEventListener("click", function (event) {
-  event.preventDefault();
-  if (validation(this) === true) {
-    alert("Форма работает!");
+// const form=document.querySelectorAll(".form")
+// form.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   if (validation(this) === true) {
+//     alert("Форма работает!");
+//   }
+// });
+
+// const validate = new JustValidate(".add-form");
+// validate.addField("#inp", [
+//   {
+//     rule: "number",
+//   },
+// ]);
+
+// function validate() {
+//   const validator = new JustValidate(".form", {
+//     errorLabelStyle: {
+//       color: "blue",
+//     },
+//   });
+//   validator.addField("#inp", [
+//     {
+//       rule: "number",
+//       errorMessage: "Введите номер студента",
+//     },
+//     {
+//       rule: "maxLength",
+//       value: 4,
+//       errorMessage: "Максимум 4 цифры",
+//     },
+//     {
+//       rule: "required",
+//       errorMessage: "Введите номер студента",
+//     },
+//   ]);
+// }
+// validate();
+
+// Рандом
+const questionText = document.querySelector(".item-text");
+// console.log(questionText);
+
+// let indexOfQuestion = questions.forEach((e) => {
+//   let indexId = e.id;
+//   console.log(indexId);
+// });
+let indexOfQuestion;
+
+let dublicateQuestions = [];
+
+function randomQestion() {
+  let randomNumber = Math.floor(Math.random() * questions.length);
+  // console.log(randomNumber);
+  // dublicateQuestions.push(randomNumber);
+
+  let dublicate = false;
+
+  if (dublicateQuestions.length > 0) {
+    dublicateQuestions.forEach((item) => {
+      if (item == randomNumber) {
+        dublicate == true;
+      }
+    });
+    if (dublicate) {
+      randomQestion();
+    } else {
+      indexOfQuestion = randomNumber;
+    }
   }
-});
+  if (dublicateQuestions == 0) {
+    indexOfQuestion = randomNumber;
+  }
+  dublicateQuestions.push(indexOfQuestion);
+  // console.log(dublicateQuestions);
+}
+randomQestion();
+// function randomQestion() {
+//   let randomNumber = Math.floor(Math.random() * questions.length);
+//   console.log(randomNumber);
+//   let dublicate = false;
+
+//   if (indexOfPage == questions.length) {
+//     console.log("Hi");
+//   } else {
+//     if (complitedAnswers.langth > 0) {
+//       complitedAnswers.forEach((item) => {
+//         if (item == randomNumber) {
+//           dublicate = true;
+//         }
+//       });
+//       if (dublicate == true) {
+//         randomQestion();
+//       } else {
+//         indexOfQuestion = randomNumber;
+//       }
+//     }
+//     if (complitedAnswers == 0) {
+//       indexOfQuestion = randomNumber;
+//     }
+//   }
+//   complitedAnswers.push(indexOfQuestion);
+//   console.log(complitedAnswers);
+// }
+// console.log(randomQestion());
+// questionText.innerHTML = randomQestion();
